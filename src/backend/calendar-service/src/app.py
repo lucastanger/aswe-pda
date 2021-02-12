d9264cdce78d603171ec5808facd4fb596635dc3
TimoStroehlein <timostroehlein@gmail.com>
2021-02-12 17:29:34 +0100

    :sparkles: aswe-4 create prototype to receive todays events

---
+++ src/backend/calendar-service/src/app.py
@@ -0,0 +1,61 @@
+from flask import Flask
+import datetime
+import pickle
+import os.path
+import json
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+app = Flask(__name__)
+
+
+@app.route('/events-today')
+def events_today():
+    credentials = authenticate()
+
+    service = build('calendar', 'v3', credentials=credentials)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    print(json.dumps(events, indent=4))
+
+    return json.dumps(events)
+
+
+def authenticate():
+    credentials = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('src/.secrets/token.pickle'):
+        with open('src/.secrets/token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'src/.secrets/credentials.json', SCOPES)
+            credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('src/.secrets/token.pickle', 'wb') as token:
+            pickle.dump(credentials, token)
+
+    return credentials
+
+
+if __name__ == '__main__':
+    app.run()
