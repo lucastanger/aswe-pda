1e6692a8fd0aa76983b67a9b2783e4fb07e90068
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-13 23:06:08 +0100

    :sparkles: aswe-1 update synthesize
    An audio file will be created.
    File can read global variables.
    Response is now an audio file.

--- src/backend/services/t2s-s2t-service/src/resources/synthesize.py
+++ src/backend/services/t2s-s2t-service/src/resources/synthesize.py
@@ -1,16 +1,52 @@
-from flask import jsonify
+# Imports.
 from flask_restful import Resource, reqparse
+from ibm_watson import ApiException
+from flask import send_file
+from pathlib import Path
+import wave
 
 
 class Synthesize(Resource):
+    '''
+        Includes post method. Receives text in json. Returns audio file.
+    '''
+
+    def __init__(self, **kwargs):
+        self.ibm = kwargs['ibm']
 
     def post(self):
-        # Extract text from body
+        # Extract text from request body.
         parser = reqparse.RequestParser()
         parser.add_argument('text', required=True, location='json')
         args = parser.parse_args(strict=True)
         text = args['text']
 
-        # Synthesize
+        # Create static audio folder if not exists.
+        Path('./static/audio').mkdir(parents=True, exist_ok=True)
 
-        return jsonify(args['text'])
+        # Create audio file.
+        audio_file = wave.open('static/audio/text2speech.wav', 'wb')
+        audio_file.setparams((1, 2, 22050, 0, 'NONE', 'Uncompressed'))
+
+        # Synthesize.
+        try:
+            audio_file.writeframes(
+                # Synthesize text to speech.
+                self.ibm.text_to_speech.synthesize(
+                    text,
+                    voice='de-DE_ErikaV3Voice',
+                    accept='audio/wav'
+                ).get_result().content)
+        except ApiException as ex:
+            # Raise exception if HTTP response code is in the 4xx and 5xx range.
+            print("Method failed with status code " +
+                  str(ex.code) + ": " + ex.message)
+
+        # Close audio file.
+        audio_file.close()
+
+        # Create response.
+        return send_file("static/audio/text2speech.wav",
+                         mimetype="audio/wav",
+                         as_attachment=True,
+                         attachment_filename="text2speech.wav")
