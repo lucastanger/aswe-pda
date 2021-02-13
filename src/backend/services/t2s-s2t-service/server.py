eefc636b1dd453d06e9a6d61af2ce0ea4f9b84c9
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-13 23:07:05 +0100

    :art: aswe-1 update server structure
    Now there are global variables.
    Setup works and routes are included

--- src/backend/services/t2s-s2t-service/server.py
+++ src/backend/services/t2s-s2t-service/server.py
@@ -1,35 +1,56 @@
-from flask import Flask
-from flask_restful import Api
+# Imports.
 from flask_script import Manager, Server
+from flask_restful import Api
+from flask import Flask
 
+# Include setup scripts.
 from src.common.setup import Setup
 
-# Import resources
-from src.resources.recognize import Recognize
+# Import resources.
 from src.resources.synthesize import Synthesize
- 
-# Global variables
-speech_to_text = None
-text_to_speech = None
+from src.resources.recognize import Recognize
+
+
+class Ibm():
+    '''
+        Ibm class. Used to have global variables.
+    '''
+
+    def __init__(self):
+        self.text_to_speech = None
+        self.speech_to_text = None
 
 
 class CustomServer(Server):
+    '''
+        CustomServer class. Used to run setup after server startup and before the first request.
+    '''
+
     def __call__(self, app, *args, **kwargs):
+        global ibm
         Setup.loadEnv()
-        Setup.authenticate()
-        Setup.setSettings()
+        ibm = Setup.authenticate(ibm)
+        ibm = Setup.setSettings(ibm)
         return Server.__call__(self, app, *args, **kwargs)
 
 
+# Create app, api and manager
 app = Flask(__name__)
-api = Api(app)
+api = Api(app,
+          prefix='/rest/api/v1'
+          )
 manager = Manager(app)
 
-# Add routes
-api.add_resource(Recognize, '/recognize')
-api.add_resource(Synthesize, '/synthesize')
+# Create global Ibm object so that every route has access to the variables.
+ibm = Ibm()
 
+# Add routes.
+api.add_resource(Synthesize, '/synthesize', resource_class_kwargs={'ibm': ibm})
+api.add_resource(Recognize, '/recognize', resource_class_kwargs={'ibm': ibm})
+
+# Create custom command.
 manager.add_command('runserver', CustomServer())
 
+# Start server.
 if __name__ == '__main__':
     manager.run()
