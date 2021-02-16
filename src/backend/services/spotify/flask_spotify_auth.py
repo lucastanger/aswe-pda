b2cfd3b6506508b887170315508682843c0a483a
Hakim Assadi <assadi.hakim@gmx.de>
2021-02-16 16:33:19 +0100

    ✨ aswe-3 introduced to feature to retrieve profile data

--- src/backend/services/spotify/flask_spotify_auth.py
+++ src/backend/services/spotify/flask_spotify_auth.py
@@ -1,10 +1,34 @@
-import base64, json, requests
+import base64, json, sys, requests
 
-SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
+try:
+    import urllib.request, urllib.error
+    import urllib.parse as urllibparse
+except ImportError:
+    import urllib as urllibparse
+
+SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize?'
 SPOTIFY_URL_TOKEN = 'https://accounts.spotify.com/api/token/'
 RESPONSE_TYPE = 'code'
 HEADER = 'application/x-www-form-urlencoded'
-REFRESH_TOKEN = ''
+
+CLIENT_ID = "4648b6cee4344e04b4c2a46d2f83a1e6"
+CLIENT_SECRET = "b5512349cd434a04858487754e11d1e6"
+
+PORT = "5565"
+CALLBACK_URL = "http://127.0.0.1"
+
+REDIRECT_URI = "{}:{}/callback/".format(CALLBACK_URL, PORT)
+SCOPE = "playlist-modify-public playlist-modify-private user-read-recently-played user-top-read"
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+}
+
+URL_ARGS = "client_id={}&response_type={}&redirect_uri={}&scope={}".format(CLIENT_ID, RESPONSE_TYPE, REDIRECT_URI, SCOPE )
+
+AUTH_URL = "{}{}".format(SPOTIFY_URL_AUTH, URL_ARGS)
 
 
 def getAuth(client_id, redirect_uri, scope):
@@ -13,35 +37,49 @@ def getAuth(client_id, redirect_uri, scope):
     return data
 
 
-def getToken(code, client_id, client_secret, redirect_uri):
+def getToken(code):
+
     body = {
         "grant_type": 'authorization_code',
-        "code": code,
-        "redirect_uri": redirect_uri,
-        "client_id": client_id,
-        "client_secret": client_secret
+        "code": str(code),
+        "redirect_uri": REDIRECT_URI
     }
 
-    encoded = base64.b64encode("{}:{}".format(client_id, client_secret))
-    headers = {"Content-Type": HEADER, "Authorization": "Basic {}".format(encoded)}
+    encoded = base64.b64encode(("{}:{}".format(CLIENT_ID, CLIENT_SECRET)).encode())
+    headers = {"Authorization": "Basic {}".format(encoded)}
 
-    post = requests.post(SPOTIFY_URL_TOKEN, params=body, headers=headers)
-    return handleToken(json.loads(post.text))
+    post_request = requests.post(SPOTIFY_URL_TOKEN, data=body, headers=headers)
+
+    respones_data = json.loads(post_request.text)
+    access_token = respones_data["access_token"]
+
+    auth_header = {"Authorization": "Bearer {}".format(access_token)}
+
+    return auth_header
 
 
-def handleToken(response):
-    auth_head = {"Authorization": "Bearer {}".format(response["access_token"])}
-    REFRESH_TOKEN = response["refresh_token"]
-    return [response["access_token"], auth_head, response["scope"], response["expires_in"]]
-
-
-def refreshAuth():
-    body = {
-        "grant_type": "refresh_token",
-        "refresh_token": REFRESH_TOKEN
+def authorize(auth_token):
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
     }
 
-    post_refresh = requests.post(SPOTIFY_URL_TOKEN, data=body, headers=HEADER)
-    p_back = json.dumps(post_refresh.text)
+    # python 3 or above
+    if sys.version_info[0] >= 3:
+        base64encoded = base64.b64encode(("{}:{}".format(CLIENT_ID, CLIENT_SECRET)).encode())
+        headers = {"Authorization": "Basic {}".format(base64encoded.decode())}
+    else:
+        base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET))
+        headers = {"Authorization": "Basic {}".format(base64encoded)}
 
-    return handleToken(p_back)
+    post_request = requests.post(SPOTIFY_URL_TOKEN, data=code_payload,
+                                 headers=headers)
+
+    # tokens are returned to the app
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+
+    # use the access token to access Spotify API
+    auth_header = {"Authorization": "Bearer {}".format(access_token)}
+    return auth_header
