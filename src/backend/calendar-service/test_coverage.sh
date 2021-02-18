2c726711cc230b6cd825c311022672c98b14d536
TimoStroehlein <timostroehlein@gmail.com>
2021-02-18 19:44:20 +0100

    :wrench: aswe-4 update coverage report directory

--- src/backend/calendar-service/test_coverage.sh
+++ src/backend/calendar-service/test_coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./venv/bin/python -m coverage report -m --omit 'venv/*'
-./venv/bin/python -m coverage html --omit 'venv/*'
+./venv/bin/python -m coverage html --omit 'venv/*' -d docs/coverage-report
