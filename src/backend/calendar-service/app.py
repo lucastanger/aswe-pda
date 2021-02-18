c99ffe4fd8f84785b605d70363307eb9c3f46bec
TimoStroehlein <timostroehlein@gmail.com>
2021-02-18 19:37:38 +0100

    :white_check_mark: aswe-4 add tests, corresponding scripts and update ignore files

--- src/backend/calendar-service/app.py
+++ src/backend/calendar-service/app.py
@@ -1,60 +1,12 @@
 from flask import Flask
-import datetime
-import pickle
-import os.path
-import json
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+from src.events import events_api
 
+# Create flask and app
 app = Flask(__name__)
 
-
-@app.route('/rest/api/v1/events-today', methods=['GET'])
-def events_today():
-    credentials = authenticate()
-
-    service = build('calendar', 'v3', credentials=credentials)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    print(json.dumps(events, indent=4))
-
-    return json.dumps(events)
-
-
-def authenticate():
-    credentials = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('.secrets/token.pickle'):
-        with open('.secrets/token.pickle', 'rb') as token:
-            credentials = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '.secrets/credentials.json', SCOPES)
-            credentials = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('.secrets/token.pickle', 'wb') as token:
-            pickle.dump(credentials, token)
-
-    return credentials
+# Create routes
+app.register_blueprint(events_api, url_prefix='/rest/api/v1/events')
 
 
 if __name__ == '__main__':
---
+++ src/backend/calendar-service/test_coverage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./venv/bin/python -m coverage report -m --omit 'venv/*'
+./venv/bin/python -m coverage html --omit 'venv/*'
