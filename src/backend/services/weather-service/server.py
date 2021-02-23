5f73bf9700b67c1dbd2cedb1e147b5a9471b75bf
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-23 20:19:42 +0100

    :sparkles: aswe-48 add current weather route

--- src/backend/services/weather-service/server.py
+++ src/backend/services/weather-service/server.py
@@ -2,10 +2,16 @@
 from flask_restful import Api
 from flask import Flask
 
+# Import resources.
+from src.resources.current_weather import CurrentWeather
+
 # Create app and api.
 app = Flask(__name__)
 api = Api(app, prefix="/rest/api/v1")
 
+# Add routes.
+api.add_resource(CurrentWeather, "/current-weather")
+
 # Start server.
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5570")
+    app.run(debug=True, host="0.0.0.0", port="5570")
