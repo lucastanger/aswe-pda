915ef836b2faafa83086b926042a77dd7fac76bf
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-24 15:24:26 +0100

    :construction: aswe-48 add forecast request. Response missing

--- src/backend/services/weather-service/server.py
+++ src/backend/services/weather-service/server.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 # Import resources.
 from src.resources.current_weather import CurrentWeather
+from src.resources.forecast import Forecast
 
 # Create app and api.
 app = Flask(__name__)
@@ -11,6 +12,7 @@ api = Api(app, prefix="/rest/api/v1")
 
 # Add routes.
 api.add_resource(CurrentWeather, "/current-weather")
+api.add_resource(Forecast, "/forecast")
 
 # Start server.
 if __name__ == "__main__":
