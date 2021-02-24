84b0503f6f121e5e711a393ba21fa48e5a3b7b9a
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-24 14:52:19 +0100

    :white_check_mark: aswe-48 add failure test for current-weather

--- src/backend/services/weather-service/tests/test_current_weather.py
+++ src/backend/services/weather-service/tests/test_current_weather.py
@@ -18,3 +18,17 @@ class CurrentWeatherTest(BaseCase):
         self.assertEqual(response.json["city_name"], city)
         self.assertIs(type(response.json["weather"]["temp"]["current"]), float)
         self.assertEqual(response.status_code, 200)
+
+    def test_failure_current_weather_get(self):
+        # Arrange
+        city = ""
+        info = "Internal server error caused by third party api."
+        code = 400
+
+        # Act
+        response = self.app.get("/rest/api/v1/current-weather?city={}".format(city))
+
+        # Assert
+        self.assertEqual(response.json["info"], info)
+        self.assertEqual(response.json["error"]["code"], code)
+        self.assertEqual(response.status_code, 500)
