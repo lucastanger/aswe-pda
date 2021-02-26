0f68b770ef0b84ee9d28dd5b0d8cae2c57b70bef
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-26 17:58:56 +0100

    :art: aswe-38 format code

--- src/backend/services/weather-service/tests/test_current_weather.py
+++ src/backend/services/weather-service/tests/test_current_weather.py
@@ -1,7 +1,3 @@
-# Imports.
-import unittest
-import json
-
 # Import setup and teardown.
 from tests.BaseCase import BaseCase
 
@@ -9,26 +5,26 @@ from tests.BaseCase import BaseCase
 class CurrentWeatherTest(BaseCase):
     def test_successful_current_weather_get(self):
         # Arrange
-        city = "Stuttgart"
+        city = 'Stuttgart'
 
         # Act
-        response = self.app.get("/rest/api/v1/current-weather?city={}".format(city))
+        response = self.app.get('/rest/api/v1/current-weather?city={}'.format(city))
 
         # Assert
-        self.assertEqual(response.json["city_name"], city)
-        self.assertIs(type(response.json["weather"]["temp"]["current"]), float)
+        self.assertEqual(response.json['city_name'], city)
+        self.assertIs(type(response.json['weather']['temp']['current']), float)
         self.assertEqual(response.status_code, 200)
 
     def test_failure_current_weather_get(self):
         # Arrange
-        city = ""
-        info = "Internal server error caused by third party api."
+        city = ''
+        info = 'Internal server error caused by third party api.'
         code = 400
 
         # Act
-        response = self.app.get("/rest/api/v1/current-weather?city={}".format(city))
+        response = self.app.get('/rest/api/v1/current-weather?city={}'.format(city))
 
         # Assert
-        self.assertEqual(response.json["info"], info)
-        self.assertEqual(response.json["error"]["code"], code)
+        self.assertEqual(response.json['info'], info)
+        self.assertEqual(response.json['error']['code'], code)
         self.assertEqual(response.status_code, 500)
