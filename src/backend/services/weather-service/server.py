e9da98a62955fd1d3a9395ed50f5b4ce0d3b5ae0
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-27 12:55:25 +0100

    :ambulance: aswe-48 remove debug

--- src/backend/services/weather-service/server.py
+++ src/backend/services/weather-service/server.py
@@ -21,4 +21,4 @@ api.add_resource(Forecast, '/forecast')
 
 # Start server.
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5570')
+    app.run(host='0.0.0.0', port='5570')
