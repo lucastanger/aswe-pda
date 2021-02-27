7622a2605dda4f656b24e55a66a6a47f0072a06c
Florian Drinkler <flo.drinkler@gmail.com>
2021-02-27 12:52:52 +0100

    :sparkles: aswe-48 add swagger

--- src/backend/services/weather-service/server.py
+++ src/backend/services/weather-service/server.py
@@ -5,15 +5,20 @@ from flask import Flask
 # Import resources.
 from src.resources.current_weather import CurrentWeather
 from src.resources.forecast import Forecast
+from src.common.swagger import Swagger
 
 # Create app and api.
 app = Flask(__name__)
-api = Api(app, prefix="/rest/api/v1")
+api = Api(app, prefix='/rest/api/v1')
+
+# Add swagger to service.
+swagger = Swagger()
+app.register_blueprint(swagger.connect_swagger())
 
 # Add routes.
-api.add_resource(CurrentWeather, "/current-weather")
-api.add_resource(Forecast, "/forecast")
+api.add_resource(CurrentWeather, '/current-weather')
+api.add_resource(Forecast, '/forecast')
 
 # Start server.
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port="5570")
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5570')
