ed8feaf49323b50589f4a22e14548a8bd5c1cb01
HakimAssadi <assadi.hakim@gmx.de>
2021-03-06 18:42:28 +0100

    :sparkles: aswe-92 first version of middleware music recap

--- src/backend/middleware/src/services/spotify_service.py
+++ src/backend/middleware/src/services/spotify_service.py
@@ -1,7 +1,45 @@
+import requests
+
+
 class SpotifyService:
     def __init__(self, parameters: dict = None):
         self.parameters = parameters
-        self.base_url = 'http://spotify-service:5565/rest/api/v1'
+        self.base_url = 'http://localhost:5565/rest/api/v1'
 
     def query(self):
-        return
+        requests.get('{}/spotify/auth'.format(self.base_url))
+
+        artist_img = []
+        artist_name = []
+        track_img = []
+        track_name = []
+
+        try:
+            artists, tracks = self.get_recap()
+            for item_a, item_t in artists['items'], tracks['items']:
+                artist_img.append(item_a['images'][0]['url'])
+                artist_name.append(item_a['name'])
+
+                track_img.append(item_t['images'][0]['url'])
+                track_name.append(item_t['name'])
+        except Exception as inst:
+            print(inst)
+            return 'Invalid response!'
+
+        print('Erfolgreich')
+        recap_artist = [artist_img, artist_name]
+        recap_tracks = [track_img, track_name]
+
+        return recap_artist, recap_tracks
+
+    def authenticate(self):
+        requests.get(f'{self.base_url}/spotify/auth')
+
+    def get_recap(self):
+        artists = requests.get(f'{self.base_url}/spotify/profile/artists')
+        tracks = requests.get(f'{self.base_url}/spotify/profile/tracks')
+        return artists.json(), tracks.json()
+
+
+service = SpotifyService()
+print(service.query())
