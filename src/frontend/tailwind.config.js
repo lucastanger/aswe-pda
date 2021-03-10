83bef37b40d2e9d70839bab29e73925eb0f43237
Luca Stanger <lstanger@gmx.de>
2021-03-10 10:18:32 +0100

    :sparkles: aswe-115 modify startup

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        turquoise: {
+          '50':  '#edf8f9',
+          '100': '#d3f7f5',
+          '200': '#a3f0e9',
+          '300': '#74eae8',
+          '400': '#23d2ca',
+          '500': '#0bb8b1',
+          '600': '#0a9b94',
+          '700': '#0f7d78',
+          '800': '#12615d',
+          '900': '#114f4b',
+        }
+      }
+    },
   },
   variants: {
     extend: {
