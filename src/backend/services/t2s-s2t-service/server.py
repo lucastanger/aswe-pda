3b1306c9ea7394c8d0af93ea31fb2a3a77bedc56
Florian Drinkler <flo.drinkler@gmail.com>
2021-03-11 22:46:37 +0100

    :zap: aswe-135 change text to speech to google api

--- src/backend/services/t2s-s2t-service/server.py
+++ src/backend/services/t2s-s2t-service/server.py
@@ -9,16 +9,16 @@ from src.common.swagger import Swagger
 
 # Create app and api.
 app = Flask(__name__)
-api = Api(app, prefix='/rest/api/v1')
+api = Api(app, prefix="/rest/api/v1")
 
 # Add swagger to service.
 swagger = Swagger()
 app.register_blueprint(swagger.connect_swagger())
 
 # Add routes.
-api.add_resource(Synthesize, '/synthesize')
-api.add_resource(Recognize, '/recognize')
+api.add_resource(Synthesize, "/synthesize")
+api.add_resource(Recognize, "/recognize")
 
 # Start server.
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5555')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5555")
