7e4bcd677b5973a14a79402b5e8ca916387f1f58
HakimAssadi <assadi.hakim@gmx.de>
2021-03-24 16:25:03 +0100

    :spakles: aswe-68 finish news api middleware

--- src/backend/services/news-service/app.py
+++ src/backend/services/news-service/app.py
@@ -1,52 +1,58 @@
 from flask import Flask, request
-from src.news import keyWordSearch, findSources, topHeadlinesCategory, everythingNews
+from src.news import key_word_search, find_sources, top_headlines_category, everything_news
 
 app = Flask(__name__)
 app.secret_key = 'some key for session'
 
 
-@app.route('/rest/api/v1/news/top/')
+@app.route('/rest/api/v1/news/top')
 def general():
-    result = topHeadlinesCategory()
+    result = top_headlines_category()
     return result
 
 
-@app.route('/rest/api/v1/news/top/category/')
+@app.route('/rest/api/v1/news/top/category')
 def top():
     if request.args['category']:
         category = request.args['category']
-        result = topHeadlinesCategory(category)
+        result = top_headlines_category(category)
     else:
         return 'Please provide a valid category!'
     return result
 
 
-@app.route('/rest/api/v1/news/everything/')
+@app.route('/rest/api/v1/news/everything')
 def everything():
-    if request.args['exclude']:
-        exclude = request.args['exclude']
-        result = everythingNews(exclude)
+    if 'source' in request.args:
+        source = request.args['source']
+        result = everything_news(source)
     else:
-        result = everythingNews()
+        result = everything_news()
     return result
 
 
 @app.route('/rest/api/v1/news/everything/search')
 def search():
-    if request.args['keyWord']:
-        keyWord = request.args['keyWord']
+    if 'keyWord' in request.args and 'source' in request.args:
+        key = request.args['keyWord']
+        source = request.args['source']
+
+        result = key_word_search(key, source)
+    elif 'keyWord' in request.args:
+        key = request.args['keyWord']
+
+        result = key_word_search(key, None)
     else:
         return (
             'Please provide a key word by using ?keyWord= at the end of your request!'
         )
 
-    result = keyWordSearch(keyWord)
     return result
 
 
-@app.route('/rest/api/v1/news/sources/')
+@app.route('/rest/api/v1/news/sources')
 def sources():
-    result = findSources()
+    result = find_sources()
     return result
 
 
