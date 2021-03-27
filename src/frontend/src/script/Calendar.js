5df9215688c2ec858f7f230eb8461619fd78364b
Luca Stanger <lstanger@gmx.de>
2021-03-27 13:41:10 +0100

    :sparkles: aswe-64 add dynamic display of appoinments

--- src/frontend/src/script/Calendar.js
+++ src/frontend/src/script/Calendar.js
@@ -27,7 +27,6 @@ function retrieveAppointmentsForTheDay() {
 
             }
 
-
         },
         error: function (error) {
             console.log(error)
@@ -38,12 +37,28 @@ function retrieveAppointmentsForTheDay() {
 
 }
 
+/**
+ *
+ * @param event
+ * @returns {string}
+ */
 function createCalendarElement(event) {
 
-    return `<a href="" target="_blank"><div class="dark:bg-black dark:bg-opacity-25 rounded-md h-20 p-2.5 shadow-2xl transition duration-500 ease-in-out transform-gpu hover:-translate-y-1 hover:scale-105 cursor-pointer">
-                            <h1 class="dark:text-gray-300 font-light pb-1">Meeting "Online Learning" Team</h1>
-                            <p class="dark:text-green-500 font-thin">14:00 - 16:00</p>
-                        </div></a>`;
+    if (event.hasOwnProperty('htmlLink') && event.hasOwnProperty('summary') && event.hasOwnProperty('start') && event.hasOwnProperty('end')) {
+        return `<a href="${event.htmlLink}" target="_blank">
+                <div class="dark:bg-black dark:bg-opacity-25 rounded-md h-20 p-2.5 shadow-2xl transition duration-500 ease-in-out transform-gpu hover:-translate-y-1 hover:scale-105 cursor-pointer">
+                    <h1 class="dark:text-gray-300 font-light pb-1">${event.summary}</h1>
+                    <p class="dark:text-green-500 font-thin">${new Date(Date.parse(event.start.dateTime)).getHours()}:${new Date(Date.parse(event.start.dateTime)).getMinutes()} - ${new Date(Date.parse(event.end.dateTime)).getHours()}:${new Date(Date.parse(event.end.dateTime)).getMinutes()} </p>
+                </div>
+            </a>`;
+    } else {
+        return `<div class="dark:bg-black dark:bg-opacity-25 rounded-md h-20 p-2.5 shadow-2xl transition duration-500 ease-in-out transform-gpu hover:-translate-y-1 hover:scale-105 cursor-pointer">
+                    <h1 class="dark:text-gray-300 font-light pb-1">${event.summary}</h1>
+                    <p class="dark:text-green-500 font-thin">${event.start.dateTime} - ${event.end.dateTime}</p>
+                </div>`;
+    }
+
+
 
 
 
--- src/frontend/src/script/Chat.js
+++ src/frontend/src/script/Chat.js
@@ -88,6 +88,8 @@ function identifyIntent(intent) {
             return handleSpotifyIntent;
         case 'news-intent':
             return handleNewsIntent;
+        case 'calendar-intent':
+            return handleCalendarIntent;
         default:
             // If intent could not get identified
             return () => {return `${intent.dialogflow.query_result.intent.display_name} does not have a according intent function`}
@@ -204,7 +206,27 @@ function handleNewsIntent(value) {
 
         }
     }
+}
 
+/**
+ *
+ * @param value
+ * @returns {HTMLDivElement}
+ */
+function handleCalendarIntent(value) {
+
+    if (value.hasOwnProperty('response')) {
+
+        let response = value.response;
+
+        if (response.constructor === Array) {
+
+            let html = `${handleMultipleCalendarData(response)}`;
+
+            return createAnswerElement(html);
+
+        }
+    }
 }
 
 /**
@@ -259,7 +281,32 @@ function handleMultipleNewsData(news) {
     ret += "</div>";
 
     return ret;
+}
 
+
+/**
+ *
+ * @param appointments
+ * @returns {string}
+ */
+function handleMultipleCalendarData(appointments) {
+
+    let ret = "";
+
+    appointments.forEach(element => {
+
+        if (element.hasOwnProperty('htmlLink') && element.hasOwnProperty('summary') && element.hasOwnProperty('start') && element.hasOwnProperty('end')) {
+
+            ret += `<a href="${element.htmlLink}" target="_blank">
+                        <div class="dark:bg-black dark:bg-opacity-25 rounded-md h-20 p-2.5 shadow-2xl transition duration-500 ease-in-out transform-gpu hover:-translate-y-1 hover:scale-105 cursor-pointer">
+                            <h1 class="dark:text-gray-300 font-light pb-1">${element.summary}</h1>
+                            <p class="dark:text-green-500 font-thin">${new Date(Date.parse(element.start.dateTime)).getHours()}:${new Date(Date.parse(element.start.dateTime)).getMinutes()} - ${new Date(Date.parse(element.end.dateTime)).getHours()}:${new Date(Date.parse(element.end.dateTime)).getMinutes()} </p>
+                        </div>
+                    </a>`;
+        }
+    })
+
+    return ret;
 }
 
 /**
