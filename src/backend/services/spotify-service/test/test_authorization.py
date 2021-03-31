a23a9c427f692fc553a15ed676d0ca06089b3086
HakimAssadi <assadi.hakim@gmx.de>
2021-03-31 17:27:41 +0200

    ✅ aswe-168 add test for spotify

--- src/backend/services/spotify-service/test/test_authorization.py
+++ src/backend/services/spotify-service/test/test_authorization.py
@@ -7,20 +7,21 @@ from app import app
 
 @pytest.fixture()
 def test_client(mocker):
-    mocker.patch('pymongo.collection.Collection.find_one', return_value=None)
-    mocker.patch('pymongo.collection.Collection.replace_one', return_value=None)
+    mocker.patch('src/backend/services/spotify-service/src/flask_spotify_auth.authorize', return_value=True)
     return app.test_client()
 
 
 class TestAuthorization:
     def test_authorization_success(self, test_client):
-        response = test_client.get('/rest/api/v1/spotify/auth', follow_redirects=True)
+        response = test_client.get('/rest/api/v1/spotify/auth')
 
         assert response.status_code == 200
 
 
 class TestCallback:
-    def test_callback_success(self, test_client):
-        response = test_client.get('/rest/api/v1/spotify/callback')
+    __code = 'testCode'
 
-        assert response.status_code == 201
+    def test_callback_success(self, test_client):
+        response = test_client.get(f'/rest/api/v1/spotify/callback?code={self.__code}')
+
+        assert response.status_code == 200
