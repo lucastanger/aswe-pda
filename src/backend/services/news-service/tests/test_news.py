ebe6a96b04701b59785e925344b7b2cded48d30a
HakimAssadi <assadi.hakim@gmx.de>
2021-04-02 16:46:47 +0200

    ✅ aswe-167 fix news service backend test

---
+++ src/backend/services/news-service/tests/test_news.py
@@ -0,0 +1,47 @@
+import pytest
+import json
+
+from app import app
+
+
+@pytest.fixture()
+def test_client():
+    return app.test_client()
+
+
+class TestNews:
+    def test_top_news_success(self, test_client):
+        response = test_client.get('/rest/api/v1/news/top')
+        json_response = json.loads(response.get_data(as_text=True))
+
+        assert json_response['status'] == 'ok'
+
+    def test_everything_news_success(self, test_client):
+        response = test_client.get('/rest/api/v1/news/everything?source=abc-news')
+        json_response = json.loads(response.get_data(as_text=True))
+
+        assert json_response['status'] == 'ok'
+
+    def test_everything_news_failure(self, test_client):
+        response = test_client.get('/rest/api/v1/news/everything?source=notASource')
+        json_response = json.loads(response.get_data(as_text=True))
+
+        assert json_response['status'] == 'error'
+
+    def test_everything_news_search_success(self, test_client):
+        response = test_client.get('/rest/api/v1/news/everything/search?source=abc-news&keyWord=testKeyWord')
+        json_response = json.loads(response.get_data(as_text=True))
+
+        assert json_response['status'] == 'ok'
+
+    def test_everything_news_search_failure(self, test_client):
+        response = test_client.get('/rest/api/v1/news/everything/search?source=notASource&keyWord=testKeyWord')
+        json_response = json.loads(response.get_data(as_text=True))
+
+        assert json_response['status'] == 'error'
+
+    def test_sources_success(self, test_client):
+        response = test_client.get('/rest/api/v1/news/sources')
+        json_response = json.loads(response.get_data(as_text=True))
+
+        assert json_response['status'] == 'ok'
