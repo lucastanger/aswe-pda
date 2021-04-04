d9ed7b40c0a3e6097e627d7049760eb2c046ce8c
Florian Drinkler <flo.drinkler@gmail.com>
2021-04-04 22:56:50 +0200

    :sparkles: aswe-170 add s2t to middleware

--- src/backend/middleware/src/routes/t2ss2t.py
+++ src/backend/middleware/src/routes/t2ss2t.py
@@ -18,3 +18,23 @@ class SynthesizeService(Resource):
             jsonify({'audio': str(base64.b64encode(response.content).decode('utf-8'))}),
             200,
         )
+
+
+@ns.route('/recognize')
+class RecognizeService(Resource):
+    def post(self):
+        wav_file = open('text2speech.wav', 'wb')
+        decode_string = base64.b64decode(request.json['audio'][22:])
+        wav_file.write(decode_string)
+        wav_file.close()
+        files = {'audio': ('audio.wav', open('text2speech.wav', 'rb'))}
+
+        response = requests.post(
+            'http://t2s-s2t-service:5555/rest/api/v1/recognize', files=files,
+        )
+
+        print(response)
+        print(response.json())
+        print(response.text)
+
+        return make_response(response.json(), 200,)
