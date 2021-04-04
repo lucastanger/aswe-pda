49f7fec012b56be9b35036288670e6c2fa387d3a
Florian Drinkler <flo.drinkler@gmail.com>
2021-04-04 23:00:02 +0200

    :bulb: aswe-170 add comments

--- src/backend/middleware/src/routes/t2ss2t.py
+++ src/backend/middleware/src/routes/t2ss2t.py
@@ -9,11 +9,13 @@ ns = Namespace('t2ss2t', description='Text to Speech and Speech to Text API')
 @ns.route('/synthesize')
 class SynthesizeService(Resource):
     def post(self):
+        # make request with text attached
         response = requests.post(
             'http://t2s-s2t-service:5555/rest/api/v1/synthesize',
             json={'text': request.json['text']},
         )
 
+        # Return base64 string from audio file
         return make_response(
             jsonify({'audio': str(base64.b64encode(response.content).decode('utf-8'))}),
             200,
@@ -23,18 +25,18 @@ class SynthesizeService(Resource):
 @ns.route('/recognize')
 class RecognizeService(Resource):
     def post(self):
+
+        # create wav file from base64 string
         wav_file = open('text2speech.wav', 'wb')
         decode_string = base64.b64decode(request.json['audio'][22:])
         wav_file.write(decode_string)
         wav_file.close()
-        files = {'audio': ('audio.wav', open('text2speech.wav', 'rb'))}
 
+        # make request with file attached
+        files = {'audio': ('audio.wav', open('text2speech.wav', 'rb'))}
         response = requests.post(
             'http://t2s-s2t-service:5555/rest/api/v1/recognize', files=files,
         )
 
-        print(response)
-        print(response.json())
-        print(response.text)
-
+        # Return text
         return make_response(response.json(), 200,)
