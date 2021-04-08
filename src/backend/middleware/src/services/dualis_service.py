14b4ec2b1f8e77694a26fd5004d926b4912afc9f
Florian Drinkler <flo.drinkler@gmail.com>
2021-04-08 10:24:07 +0200

    :sparkles: aswe-113 add dualis to middleware

--- src/backend/middleware/src/services/dualis_service.py
+++ src/backend/middleware/src/services/dualis_service.py
@@ -1,7 +1,32 @@
+from src.util.mongodb import MongoDB
+import requests
+
+
 class DualisService:
     def __init__(self, parameters: dict = None):
         self.parameters = parameters
-        self.base_url = 'http://dualis-service:5550/rest/api/v1'
+        self.base_url = "http://dualis-service:5550/rest/api/v1"
 
     def query(self):
-        return
+
+        # Load dualis login data from config
+        config = (
+            MongoDB.instance()
+            .db["configuration"]
+            .find_one(
+                {
+                    "dualis": {"$exists": True},
+                }
+            )
+        )
+
+        username = config["dualis"]["_username"]
+        password = config["dualis"]["_password"]
+
+        # Get grades from dualis api
+        response = requests.get(
+            f"{self.base_url}/grades", json={"user": username, "password": password}
+        )
+
+        # Return response
+        return response.json()
