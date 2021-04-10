2780789c168e01bc905af75f9c215e4af199ca91
HakimAssadi <assadi.hakim@gmx.de>
2021-04-10 14:17:55 +0200

    ✅ aswe-167 add news service middle ware tests

--- src/backend/middleware/test.sh
+++ src/backend/middleware/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -m pytest --cov=src --cov-report=xml --cov-report html:docs/coverage --cov-report term tests/services/test_spotify_service.py
+python -m pytest --cov=src --cov-report=xml --cov-report html:docs/coverage --cov-report term tests/
