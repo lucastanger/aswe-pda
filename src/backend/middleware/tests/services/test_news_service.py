a66e75422e759fdb12a4a357e292ce2f1ed70030
HakimAssadi <assadi.hakim@gmx.de>
2021-04-10 14:16:46 +0200

    ✅ aswe-167 add news service middle ware tests

--- src/backend/middleware/tests/services/test_news_service.py
+++ src/backend/middleware/tests/services/test_news_service.py
@@ -1,4 +1,6 @@
+import json
 from unittest import mock
+from requests import Response
 
 import pytest
 
@@ -6,12 +8,27 @@ from src.services.news_service import NewsService
 
 BASE_URL = 'http://news-service:5575/rest/api/v1'
 PAPER_ID = 'abc-news'
-CATEGORY = 'sports'
+CATEGORY = ['sports']
+
+test_response = Response()
+test_response._content = b'{"sources": [{"name": "testName", "url": "testURL", "id": "testID"}], "articles": [{"title": "testTitle", "urlToImage": "testURLToImage", "url": "testURL"}]}'
+test_response.status_code = 200
+
+test_response_no_url = Response()
+test_response_no_url._content = b'{"articles": [{"title": "testTitle", "url": "testURL"}]}'
+test_response_no_url.status_code = 200
 
 
 @pytest.fixture
-def service(mocker, parameters):
-    mocker.patch('src.backend.middleware.src.services.news_service.NewsService.get_db_info', paper_id=PAPER_ID, category=CATEGORY)
+def service_with_db_info(mocker, parameters):
+    mocker.patch.object(NewsService, 'get_db_info', return_value=[PAPER_ID, CATEGORY])
+    news_service = NewsService(parameters)
+    return news_service
+
+
+@pytest.fixture
+def service_without_db_info(mocker, parameters):
+    mocker.patch.object(NewsService, 'get_db_info', return_value=[None, None])
     news_service = NewsService(parameters)
     return news_service
 
@@ -22,30 +39,64 @@ class TestNewsService:
     __type_sources = 'sources'
     __search = 'someKeyWord'
 
-    @mock.patch('request.get')
+    @mock.patch('requests.get', return_value=test_response_no_url)
     @pytest.mark.parametrize('parameters', [{'type': __type_top}])
-    def test_top_news_success(self, mocked_get, service):
-        _response = service.query()
+    def test_top_news_category_success(self, mocked_get, service_with_db_info):
+        _response = service_with_db_info.query()
 
-        mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_top}/category?category={CATEGORY}')
+        mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_top}/category?category={CATEGORY[0]}')
 
-    @mock.patch('request.get')
+        assert _response[0]['img'] == 'https://www.bag.admin.ch/bag/de/home/das-bag/aktuell/news/news-02-09-2020/_jcr_content/image.imagespooler.png/1603898250046/588.1000/Icons-18.png'
+
+    @mock.patch('requests.get', return_value=test_response)
+    @pytest.mark.parametrize('parameters', [{'type': __type_top}])
+    def test_top_news_success(self, mocked_get, service_without_db_info):
+        _response = service_without_db_info.query()
+
+        mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_top}')
+
+        assert _response[0]['title'] == 'testTitle'
+
+    @mock.patch('requests.get')
     @pytest.mark.parametrize('parameters', [{'type': __type_everything}])
-    def test_everything_news_success(self, mocked_get, service):
-        _response = service.query()
+    def test_everything_news_success(self, mocked_get, service_without_db_info):
+        _response = service_without_db_info.query()
+
+        mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_everything}')
+
+    @mock.patch('requests.get')
+    @pytest.mark.parametrize('parameters', [{'type': __type_everything}])
+    def test_everything_news_paper_success(self, mocked_get, service_with_db_info):
+        _response = service_with_db_info.query()
 
         mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_everything}?source={PAPER_ID}')
 
-    @mock.patch('request.get')
+    @mock.patch('requests.get')
     @pytest.mark.parametrize('parameters', [{'type': __type_everything, 'search': __search}])
-    def test_everything_search_news_success(self, mocked_get, service):
-        _response = service.query()
+    def test_everything_news_search_and_paper_success(self, mocked_get, service_with_db_info):
+        _response = service_with_db_info.query()
 
         mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_everything}/search?keyWord={self.__search}&source={PAPER_ID}')
 
-    @mock.patch('request.get')
+    @mock.patch('requests.get')
+    @pytest.mark.parametrize('parameters', [{'type': __type_everything, 'search': __search}])
+    def test_everything_news_search_success(self, mocked_get, service_without_db_info):
+        _response = service_without_db_info.query()
+
+        mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_everything}')
+
+    @mock.patch('requests.get', return_value=test_response)
     @pytest.mark.parametrize('parameters', [{'type': __type_sources}])
-    def test_sources_success(self, mocked_get, service):
-        _response = service.query()
+    def test_sources_success(self, mocked_get, service_with_db_info):
+        _response = service_with_db_info.query()
 
         mocked_get.assert_called_with(f'{BASE_URL}/news/{self.__type_sources}')
+
+        assert _response[0]['name'] == 'testName'
+
+    @pytest.mark.parametrize('parameters', [{'type': 'failureType'}, {'noType': 'noType'}])
+    def test_news_failure(self, service_with_db_info):
+        _response = service_with_db_info.query()
+
+        assert _response == 'Please provide a valid type!' or _response == 'Please provide a type!'
+
